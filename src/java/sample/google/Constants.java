7f2807b7a41009a689434e9cb22d7ec6a2e7ee30
ThaoNguyen151 <tpthaonguyen04@gmail.com>
2024-11-20 11:27:59 +0700

    project commit

--- src/java/sample/google/Constants.java
+++ src/java/sample/google/Constants.java
@@ -11,10 +11,6 @@ package sample.google;
  */
 public class Constants {
 
-    public static String GOOGLE_CLIENT_ID = "938662090458-j9sibk247sg7lj1dscdtvus3n8uapg8v.apps.googleusercontent.com";
-
-    public static String GOOGLE_CLIENT_SECRET = "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";
-
     public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/SE180486_Assignment_T4S3/LoginGoogleController";
 
     public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
